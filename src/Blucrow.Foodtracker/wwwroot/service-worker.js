0d670a436986a6be64d46e8f720413610ff10e02
Blucrow <74022166+Blucrow@users.noreply.github.com>
2025-05-18 00:53:27 +0200

    modified script

--- src/Blucrow.Foodtracker/wwwroot/service-worker.js
+++ src/Blucrow.Foodtracker/wwwroot/service-worker.js
@@ -2,10 +2,3 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-navigator.serviceWorker.register('/Simplefoodtracker/wwwroot/service-worker.js', {
-    scope: '/Simplefoodtracker/'
-}).then(registration => {
-    console.log('Service worker registered with scope:', registration.scope);
-}).catch(error => {
-    console.error('Service worker registration failed:', error);
-});
