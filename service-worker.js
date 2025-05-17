e02d20703c5142b1c6b828f518ba9d80dd19aa94
Blucrow <74022166+Blucrow@users.noreply.github.com>
2025-05-17 18:17:10 +0000

    Deploying to gh-pages from  @ ab656c8902942f35300d4b190227c0e6b1bc07e7 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: pv4UH0wV */
+/* Manifest version: r4vD3c1m */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
