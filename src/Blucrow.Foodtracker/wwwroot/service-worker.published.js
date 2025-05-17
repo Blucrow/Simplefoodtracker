9a648ead8e9eb93ab753d3fc7fa96f7819eceb19
Blucrow <74022166+Blucrow@users.noreply.github.com>
2025-05-17 20:24:23 +0200

    modified service worker

--- src/Blucrow.Foodtracker/wwwroot/service-worker.published.js
+++ src/Blucrow.Foodtracker/wwwroot/service-worker.published.js
@@ -10,9 +10,11 @@ const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
-
+const excludedAssets = [
+    '_content/ZXingBlazor/lib/zxing/zxing.min.js'
+];
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
-const base = "/";
+const base = "/Simplefoodtracker/";
 const baseUrl = new URL(base, self.origin);
 const manifestUrlList = self.assetsManifest.assets.map(asset => new URL(asset.url, baseUrl).href);
 
