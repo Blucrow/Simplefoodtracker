e49eb3841106c09bd2e54690d97fd9c94e6ebdb6
Blucrow <74022166+Blucrow@users.noreply.github.com>
2025-05-21 18:27:42 +0200

    IsDevelopment for basepath

--- src/Blucrow.Foodtracker/Program.cs
+++ src/Blucrow.Foodtracker/Program.cs
@@ -10,6 +10,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IndexedDbAccessor>();
-builder.Services.AddScoped(sp => new BasePathHelper { BasePath = builder.HostEnvironment.BaseAddress });
+if (builder.HostEnvironment.IsDevelopment())
+{
+    builder.Services.AddScoped(sp => new BasePathHelper { BasePath = builder.HostEnvironment.BaseAddress });
+
+}
+else
+    builder.Services.AddScoped(sp => new BasePathHelper { BasePath = "https://blucrow.github.io/Simplefoodtracker/" });
+
 builder.Services.AddScoped<CustomNavigationService>();
 await builder.Build().RunAsync();
